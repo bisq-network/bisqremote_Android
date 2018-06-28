426339b3593ddda28865a9d4ded961da5ae3d9c2
Joachim Neumann <joachim@mindo.io>
2018-06-29 01:31:59 +0200

    receiving encrypted message

--- Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseMessagingService.java
+++ Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseMessagingService.java
@@ -4,25 +4,29 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class BisqFirebaseMessagingService extends FirebaseMessagingService {
+    public static final String BISQ_MESSAGE_ANDROID_MAGIC = "BisqMessageAndroid";
 
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        String notifyData = remoteMessage.getData().get("score");
-
-        if(notifyData.contains("|")){
-            String[] itens = notifyData.split("\\|");
-            notifyData = itens[0];
+        String encrypted = remoteMessage.getData().get("encrypted");
+        if (encrypted != null) {
+            processNotification(encrypted);
         }
+    }
 
-
-        String notifyType = remoteMessage.getData().get("notifType");
-        String title = remoteMessage.getData().get("title");
-        String message = remoteMessage.getData().get("body");
-
-//        if(!isAppInForeground(App.getContext())){
-//            sendNotification(title, message, notifyData, notifyType);
-//        }
+    private void processNotification(String e) {
+        String key;
+        String enc;
+        String[] array = e.split("\\|");
+        if (array.length == 3) {
+            if (array[0].equals(BISQ_MESSAGE_ANDROID_MAGIC)) {
+                if (array[1].length() == 16) {
+                    key = array[2];
+                    enc = array[3];
+                }
+            }
+        }
     }
 }
