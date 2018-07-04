434c6d03c936fd355dffa9500e025d2049778a6e
Joachim Neumann <joachim@mindo.io>
2018-07-04 09:06:52 +0200

    Catching decrypt errors

--- Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseMessagingService.kt
+++ Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseMessagingService.kt
@@ -55,34 +55,36 @@ class BisqFirebaseMessagingService : FirebaseMessagingService() {
                         }
 
                         if (success != null) {
+                            if (success.startsWith("{\"times")) {
+                                // TODO add to database
+                                val gsonBuilder = GsonBuilder()
+                                gsonBuilder.registerTypeAdapter(Date::class.java, DateDeserializer())
+                                val gson = gsonBuilder.create()
+                                val newNotification = gson.fromJson<RawBisqNotification>(success, RawBisqNotification::class.java)
+                                val notificationRepository = NotificationRepository(this)
+                                notificationRepository.insert(newNotification)
 
-                            // TODO add to database
-                            val gsonBuilder = GsonBuilder()
-                            gsonBuilder.registerTypeAdapter(Date::class.java, DateDeserializer())
-                            val gson = gsonBuilder.create()
-                            val newNotification = gson.fromJson<RawBisqNotification>(success, RawBisqNotification::class.java)
-                            val notificationRepository = NotificationRepository(this)
-                            notificationRepository.insert(newNotification)
-
-                            val intent = Intent(this, TransferCodeActivity::class.java)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                            // Create the pending intent to launch the activity
-                            val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                                    PendingIntent.FLAG_ONE_SHOT)
+                                val intent = Intent(this, TransferCodeActivity::class.java)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                // Create the pending intent to launch the activity
+                                val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                                        PendingIntent.FLAG_ONE_SHOT)
 
 
-                            val notificationBuilder = NotificationCompat.Builder(this, "Bisq")
-                                    .setSmallIcon(R.drawable.help)
-                                    .setContentTitle("bisq")
-                                    .setContentText(success)
-                                    .setAutoCancel(true)
-                                    .setContentIntent(pendingIntent)
+                                val notificationBuilder = NotificationCompat.Builder(this, "Bisq")
+                                        .setSmallIcon(R.drawable.help)
+                                        .setContentTitle("bisq")
+                                        .setContentText(success)
+                                        .setAutoCancel(true)
+                                        .setContentIntent(pendingIntent)
 
-                            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                                val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-                            notificationManager.notify(System.currentTimeMillis().toInt(), notificationBuilder.build())
-
-                            Log.i("Bisq", "decrypted json: $success")
+                                notificationManager.notify(System.currentTimeMillis().toInt(), notificationBuilder.build())
+                                Log.i("Bisq", "added to database: $success")
+                            } else {
+                                Log.i("Bisq", "ERROR decrypting json: $success")
+                            }
                         }
                     }
                 }
