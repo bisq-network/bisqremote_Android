358475596a98b083ac9d49666527e67a43a8f507
Joachim Neumann <joachim@mindo.io>
2018-07-05 18:44:28 +0200

    after upgrading kotlin

--- Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseInstanceIdService.kt
+++ Bisq/src/main/java/com/joachimneumann/bisq/BisqFirebaseInstanceIdService.kt
@@ -2,23 +2,20 @@ package com.joachimneumann.bisq
 
 import android.util.Log
 
-import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.iid.FirebaseInstanceIdService
-import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.FirebaseMessagingService
 
 
-class BisqFirebaseInstanceIdService : FirebaseInstanceIdService() {
+class BisqFirebaseInstanceIdService : FirebaseMessagingService() {
 
-    override fun onTokenRefresh() {
-        // Get updated InstanceID token.
-        val refreshedToken = FirebaseInstanceId.getInstance().token
-        Log.d(TAG, "Refreshed token: " + refreshedToken!!)
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+        Log.d(TAG, "Refreshed token: " + p0)
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
         val phone = Phone.getInstance(this)
-        phone.apsToken = refreshedToken
+        phone.apsToken = p0
     }
 
     companion object {
