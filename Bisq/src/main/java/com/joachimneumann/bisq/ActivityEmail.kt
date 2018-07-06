2e17d1981505c5ef4f788f507578dce41c27d7c6
Joachim Neumann <joachim@mindo.io>
2018-07-06 20:06:50 +0200

    Adding resend email button

--- Bisq/src/main/java/com/joachimneumann/bisq/ActivityEmail.kt
+++ Bisq/src/main/java/com/joachimneumann/bisq/ActivityEmail.kt
@@ -1,5 +1,6 @@
 package com.joachimneumann.bisq
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -8,12 +9,27 @@ import android.widget.TextView
 class ActivityEmail : AppCompatActivity() {
 
     private lateinit var status: TextView
+    private lateinit var resend_email_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_email)
         status = bind(R.id.register_email_status_textview)
         status.setText(getString(R.string.register_email_status)+": not yet registered")
+        resend_email_button = bind(R.id.resend_email_button)
+        resend_email_button.setOnClickListener { createEmail() }
+        createEmail()
+    }
+
+    private fun createEmail() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/html"
+        var phone: Phone = Phone.getInstance(this)
+        val x = phone.apsToken
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+        val emailBody = getString(R.string.email_content1) + phone.phoneID() + getString(R.string.email_content2)
+        intent.putExtra(Intent.EXTRA_TEXT, emailBody)
+        startActivity(Intent.createChooser(intent, "Send Email"))
     }
 
 }
--- Bisq/src/main/java/com/joachimneumann/bisq/ActivityRegisterInstructions.kt
+++ Bisq/src/main/java/com/joachimneumann/bisq/ActivityRegisterInstructions.kt
@@ -21,22 +21,6 @@ class ActivityRegisterInstructions : AppCompatActivity() {
         webcamButton = bind(R.id.register_webcam_button)
         webcamButton.setOnClickListener { startActivity(Intent(this,ActivityQR::class.java)) }
         emailButton = bind(R.id.register_email_button)
-        emailButton.setOnClickListener { createEmail() }
-    }
-
-    public fun createEmail() {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "text/html"
-        var phone = Phone.getInstance(this)
-        val x = phone.apsToken
-        intent.putExtra(Intent.EXTRA_EMAIL, "your_email_address")
-        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
-        val emailBody = getString(R.string.email_content1) + phone.phoneID() + getString(R.string.email_content2)
-        intent.putExtra(Intent.EXTRA_TEXT, emailBody)
-        startActivityForResult(Intent.createChooser(intent, "Send Email"), 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        startActivity(Intent(this,ActivityEmail::class.java))
+        emailButton.setOnClickListener { startActivity(Intent(this,ActivityEmail::class.java)) }
     }
 }
